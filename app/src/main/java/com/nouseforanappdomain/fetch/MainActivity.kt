703a0636e026edc3d82002dfed0bdedcb9485b0e
James Anderson <silatjedi@gmail.com>
2024-06-25 13:13:13 -0600

    add recyclerview

--- app/src/main/java/com/nouseforanappdomain/fetch/MainActivity.kt
+++ app/src/main/java/com/nouseforanappdomain/fetch/MainActivity.kt
@@ -3,6 +3,9 @@ package com.nouseforanappdomain.fetch
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nouseforanappdomain.fetch.adapter.ListAdapter
 import com.nouseforanappdomain.fetch.network.ListApi
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +15,10 @@ class MainActivity : AppCompatActivity() {
 
         ListApi.getList(
             { listItems ->
-                println(listItems.toString())
+                findViewById<RecyclerView>(R.id.list)?.apply {
+                    layoutManager = LinearLayoutManager(this@MainActivity)
+                    adapter = ListAdapter(listItems)
+                }
             },
             {
                 Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
